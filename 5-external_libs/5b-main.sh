cdd6f4979b35ba3e5b25c8205977eb35979b078c
= <ciaabcdefg@gmail.com>
2024-02-18 16:58:59 +0700

    reorganized 5-libs

---
+++ 5-external_libs/5b-main.sh
@@ -0,0 +1,6 @@
+# "." is like "import ./5a-math.sh"
+# or "import 5a-math.sh"
+. 5a-math.sh
+
+echo $PI
+echo $(abs -20)
--- 5-libs.sh
+++
@@ -1,6 +0,0 @@
-# import ./5a-math.sh
-# import 5a-math.sh
-. 5a-math.sh
-
-echo $PI
-echo $(abs -20)
